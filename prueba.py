358a9898023793943a87092e71219c4587953f75
Nicolas <cerqueras@uninorte.edu.co>
2021-09-17 09:54:35 -0500

    Primer proyecto

---
+++ prueba.py
@@ -0,0 +1,17 @@
+""" set = {1, 2, 3, 4}
+
+set.remove(2)
+set.discard(3)
+set.add(7) """
+""" set.clear() """
+#print(set)
+
+A = {1,2,3,4,5}
+
+B = {4,5,8,9,10}
+
+print(A & B)
+print(A | B)
+print(A - B)
+print(A ^ B)
+
